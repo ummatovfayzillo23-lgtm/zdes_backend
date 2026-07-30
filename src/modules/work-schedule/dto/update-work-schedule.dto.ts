2d37515efe81347c97c44170466219d509dd0063
tuxtasinboyev <omadbektuxtasinboyev06@gmail.com>
2026-07-30 20:00:20 +0500

    response tugrilandi:

--- src/modules/work-schedule/dto/update-work-schedule.dto.ts
+++ src/modules/work-schedule/dto/update-work-schedule.dto.ts
@@ -2,3 +2,4 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateWorkScheduleDto } from './create-work-schedule.dto';
 
 export class UpdateWorkScheduleDto extends PartialType(CreateWorkScheduleDto) {}
+
